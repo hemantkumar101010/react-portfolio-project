6c2c89d3c3e742bf92afd64d8a9f19febf56ce3f
hemantkumar101010 <heman5502@gmail.com>
2021-10-17 19:18:25 +0530

    Navbar Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,15 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
 <div className="container">
-        <a class="navbar-brand" href="#"><img className="logo" src ={logo} alt="3coder logo" /></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a className="navbar-brand" href="#"><img className="logo" src ={logo} alt="3coder logo" /></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         < FontAwesomeIcon icon={faBars} style={{color : "#fff"}}/>
         </button>
       
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
               <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
             </li>
