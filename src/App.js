9f63fc73bff59bccd40838e624429797d58f869b
hemantkumar101010 <heman5502@gmail.com>
2021-10-19 20:56:35 +0530

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './components/Header';
+import Particles from 'react-particles-js';
+
 function App() {
   return (
-     <Navbar />
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          "particles": {
+            "number": {
+              "value": 50
+            },
+            "size": {
+              "value": 3
+            }
+          },
+          "interactivity": {
+            "events": {
+              "onhover": {
+                "enable": true,
+                "mode": "repulse"
+              }
+            }
+          }
+        }}
+
+      />
+      <Navbar />
+      <Header />
+
+    </>
   );
 }
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Typed from "react-typed";
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>programmers community on instagram</h1>
+                <Typed 
+                className="typed-text"
+                strings={["Web Design", "Web Development", "Promotional Ads", "Social Media Marketing"]}
+                typeSpeed={40}
+                backSpeed={100}
+                loop
+                />
+                <a href="a" className="btn-main-offer"> Contact Me </a>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
