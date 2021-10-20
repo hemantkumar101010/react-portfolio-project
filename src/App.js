3e80ad15b5069078269e1c0c2d49351a0174ec26
hemantkumar101010 <heman5502@gmail.com>
2021-10-20 20:30:54 +0530

    Services completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/Header';
 import Particles from 'react-particles-js';
 import AboutMe from './components/AboutMe';
+import Services from './components/Services';
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
       <Navbar />
       <Header />
       <AboutMe />
+      <Services />
 
     </>
   );
