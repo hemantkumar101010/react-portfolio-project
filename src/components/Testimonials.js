0d28e97f165a992c88974af51b106ee4b2bd2e4e
hemantkumar101010 <heman5502@gmail.com>
2021-10-24 19:11:59 +0530

    Testimonials completed

---
+++ src/components/Testimonials.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import TestimonialsCarousel from './TestimonialsCarousel';
+
+
+const Testimonials = () => {
+    return (
+        <div className="testimonials">
+            <h1>my happy clients</h1>
+            <div className="container">
+                <div className="testimonials-content">
+                    <TestimonialsCarousel />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Testimonials;
